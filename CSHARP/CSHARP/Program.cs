5a84d01f5fa4a279d9b2fa35583adb00a890c5bf
Nurlan M. Musayev <nurlanmm@code.local>
2023-09-10 14:15:55 +0400

    CS tasks

---
+++ CSHARP/CSHARP/Program.cs
@@ -0,0 +1,119 @@
+﻿// TASK01
+
+//int n = 5;
+
+//if(n % 2 == 0)
+//{
+//    Console.WriteLine("Cutdur");
+//}
+//else
+//{
+//    Console.WriteLine("Tekdir");
+//}
+
+
+//TASK02
+
+//int m = 4;
+
+//for(int i = 0; i <= m; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+
+//TASK03
+
+//int m = 5;
+
+//int sum = 0;
+
+//for(int i = 0; i <= m; i++)
+//{
+//    sum += i;
+
+//}
+
+//Console.WriteLine(sum);
+
+
+
+
+//TASK04
+
+//int m = 5;
+
+//int count = 0;
+
+//for (int i = 0; i <= m; i++)
+//{
+//    count++;
+
+//}
+
+//Console.WriteLine(count);
+
+
+
+
+//TASK05
+
+//int n = 3;
+
+//int m = 10;
+
+//int sum = 0;
+
+
+//for(int i = n; i <= m; i++)
+//{
+//    if(i%2 == 0)
+//    {
+//        sum += i;
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+
+
+
+//TASK06
+
+
+//int n = 5;
+
+//int fact = 1;
+
+//for(int i = 1; i <= n; i++)
+//{
+//    fact *= i;
+//}
+
+
+//Console.WriteLine(fact);
+
+
+
+
+//TASK07
+
+
+//int[] arr = { 2, 4, 6, 8 ,5,7};
+
+
+//int multipl = 1;
+
+//for(int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 == 0)
+//    {
+//        multipl *= arr[i];
+//    }
+//}
+
+
+//Console.WriteLine(multipl);
+
